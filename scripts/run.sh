0323ec72b1e508282bc4256f1d954036a4cd4079
QuangPham <quangg2012@gmail.com>
2022-10-18 00:50:09 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,7 +12,7 @@ for m in ${methods[*]}; do
 CUDA_VISIBLE_DEVICES=0 python -u main.py --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data ETTh2 --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 6 --learning_rate 1e-3 --online_learning 'full'
 CUDA_VISIBLE_DEVICES=0 python -u main.py --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data ETTm1 --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 6 --learning_rate 1e-3 --online_learning 'full'
 CUDA_VISIBLE_DEVICES=0 python -u main.py  --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data WTH --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 6 --learning_rate 1e-3 --online_learning 'full'
-CUDA_VISIBLE_DEVICES=0 python -u main.py --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data ECL --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 6 --learning_rate 1e-3 --online_learning 'full'
+CUDA_VISIBLE_DEVICES=0 python -u main.py --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data ECL --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 6 --learning_rate 3e-3 --online_learning 'full'
 done
 done
 done
@@ -24,7 +24,7 @@ for bsz in ${bszs[*]}; do
 for len in ${lens[*]}; do
 for m in ${methods[*]}; do
 echo $m $l
-CUDA_VISIBLE_DEVICES=0 python -u main.py --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data Traffic --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 6 --learning_rate 1e-3 --online_learning 'full'
+CUDA_VISIBLE_DEVICES=0 python -u main.py --method $m --root_path ./data/ --n_inner $n --test_bsz $bsz --data Traffic --features M --seq_len 60 --label_len 0 --pred_len $len --des 'Exp' --itr $i --train_epochs 6 --learning_rate 3e-3 --online_learning 'full'
 done
 done
 done
